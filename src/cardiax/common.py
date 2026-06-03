23cd6c06a06661fc9d7c61b0e751097a41698525
Gabriel Peery <peerygabriel@gmail.com>
2026-06-03 14:16:24 -0500

    Only enable timing functionality during debugging to avoid possible performance drop

--- src/cardiax/common.py
+++ src/cardiax/common.py
@@ -4,14 +4,17 @@ import time
 from functools import wraps
 
 from cardiax import logger
+from logging import DEBUG
 
 # A simpler decorator for printing the timing results of a function
 def timeit(func):
-    """A decorator to wrap a function for timing purposes.
+    """A decorator that times a function call when debugging
 
-    Utilizes JAX's recommend block-until-ready to ensure JIT'd
-    compilation time is measured rather than initial tracer dispatch
-    time.
+    If cardiax.logger has DEBUG-level logging enabled, utilizes JAX's
+    recommend block-until-ready to ensure JIT'd compilation time is
+    measured rather than initial tracer dispatch time.
+
+    Otherwise, calls the function without timing.
 
     Args:
         func (callable): The function to be wrapped to log times
@@ -22,12 +25,21 @@ def timeit(func):
 
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        jax.block_until_ready(result)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        logger.debug(f'Function {func.__name__} took {total_time:.4f} seconds')
+        if logger.isEnabledFor(DEBUG):
+            # Only go through the trouble of timing if debugging
+            start_time = time.perf_counter()
+
+            result = func(*args, **kwargs)
+            jax.block_until_ready(result)
+
+            end_time = time.perf_counter()
+
+            total_time = end_time - start_time
+            logger.debug(f'Function {func.__name__} took {total_time:.4f} seconds')
+        else:
+            # Pass through silently when not debugging
+            result = func(*args, **kwargs)
+
         return result
 
     return timeit_wrapper
--- test/unittests/test_timeit.py
+++ test/unittests/test_timeit.py
@@ -1,13 +1,13 @@
 import unittest
 from cardiax import logger
-from logging import DEBUG
+from logging import DEBUG, INFO
 from cardiax.common import timeit
 from jax.random import normal
 from jax import jit
 import jax.numpy as jnp
 
 
-class testsTimeit(unittest.TestCase):
+class testsTimeitDebugging(unittest.TestCase):
 
     def setUp(self) -> None:
         def my_func(x):
@@ -26,6 +26,9 @@ class testsTimeit(unittest.TestCase):
         x = normal(jnp.array([1337, 1337], dtype=jnp.uint32), (2048, 2048))
 
         with self.assertLogs(logger, DEBUG) as cm:
+            # Make sure logger can be level-adjusted any time
+            logger.setLevel(DEBUG)
+
             y = timed(x)
 
         # Slightly fragile: second-to-last word in single logger
@@ -34,6 +37,44 @@ class testsTimeit(unittest.TestCase):
         time_output = cm.output[0]
         time_float = float(time_output.split(' ')[-2])
         self.assertGreater(time_float, 0.0)
+
+        self.assertTrue((y == x**2 + x + 1).all())
+
+
+    def test_jitted(self):
+        """Tests timeit logs given JIT'd function"""
+        self._test_variant(self.func_jit)
+        
+
+    def test_not_jitted(self):
+        """Tests timeit logs given not-JIT'd function"""
+        self._test_variant(self.func_nojit)
+
+
+class testsTimeitNotDebugging(unittest.TestCase):
+
+    def setUp(self) -> None:
+        def my_func(x):
+            return x ** 2 + x + 1
+
+        self.func_nojit = my_func
+        self.func_jit = jit(my_func)
+
+        return super().setUp()
+
+
+    def _test_variant(self, the_func):
+        timed = timeit(the_func)
+
+        # Intentionally big and slow
+        x = normal(jnp.array([1337, 1337], dtype=jnp.uint32), (2048, 2048))
+
+        # It should pass through in this case
+        with self.assertNoLogs(logger, DEBUG):
+            # Make sure logger can be level-adjusted any time
+            logger.setLevel(INFO)
+
+            y = timed(x)
 
         self.assertTrue((y == x**2 + x + 1).all())
 
